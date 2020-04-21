92809f21c48641e95908e0b6ec143f97a55d2710
Monseg <mons@mons.ws>
2020-04-21 10:39:07 +0300

    Add files via upload

---
+++ new_bot.php
@@ -0,0 +1,39 @@
+<?php
+header("content-type: text/html; charset: UTF-8");
+
+## Блок настроек photosession.tips
+# https://photosession.tips/page/api/
+
+
+$url = 'https://photosession.tips/api/api.php?'; #Ссылка на запрос к сервису
+$type = 'bot'; # тип запроса к сервису
+$apikey = ''; # ваш API ключ сервиса  (https://photosession.tips/open_api/)
+$get_idea = file_get_contents('https://photosession.tips/api/api.php?type=' . $type . '&key=' . $apikey . ''); # отправка запроса к сервису 
+$res = json_decode($get_idea);
+$idea_message = $res->message; # текст с идеей
+$idea_image_src = 'https:' . $res->image_src; # ссылка на пример фотографии
+## Блок настроек ICQ
+# https://icq.com/botapi/
+
+
+$icq_bot_token = ''; # ICQ токен (https://icq.com/botapi/botTutorial.html)
+$icq_chatId = 'photosession.tips'; # название канала,куда производится постинг
+$icq_bot_text = 'Идея для фотографии! @photosession.tips «' . $idea_message . '» '; # текст описание для изображения
+$postdata = array(
+				'token' => $icq_bot_token,
+				'chatId' => $icq_chatId,
+				'caption' => $icq_bot_text,
+				'file' => file_get_contents($idea_image_src)
+);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://api.icq.net/bot/v1/messages/sendFile?');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+$icq_bot_resp = curl_exec($ch);
+curl_close($ch);
+
+echo $icq_bot_resp; # просмотр ответа сервера, при отсутствии нужды можно закомментировать
+
+?>
